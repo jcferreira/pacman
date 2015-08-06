89f8f5ac3caa4d6f199724be5fe42e8c98dc3e81
Julio Ferreira <jcferreira@amil.com.br>
2015-08-06 13:06:36 -0300

    Criando estrutura inicial para pacman

---
+++ src/main/java/br/com/pacman/App.java
@@ -0,0 +1,13 @@
+package br.com.pacman;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
