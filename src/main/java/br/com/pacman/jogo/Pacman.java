9d1073f640979228d220afef4c1b48e45541cc65
Julio <jcferreira@gmail.com>
2015-08-08 21:38:01 -0300

    estrutura para atribuir tipo de linha ao jogo

--- src/main/java/br/com/pacman/jogo/Pacman.java
+++ src/main/java/br/com/pacman/jogo/Pacman.java
@@ -1,5 +1,6 @@
 package br.com.pacman.jogo;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -22,35 +23,37 @@ public class Pacman {
 	private LeitorArquivo leitorArquivo = new LeitorArquivo();
 	
 	public void iniciarJogo() {
+		List<Jogo> jogos = montarJogos();
 		
 	}
 	
-	private void montarJogos() {
+	private List<Jogo> montarJogos() {
 		List<String> linhas = leitorArquivo.carregarLinhasArquivo();
+		List<Jogo> jogos = new ArrayList<Jogo>();
 		
+		Jogo jogo = new Jogo();
 		for (String linha : linhas) {
-			
+			if (analisarLinha(linha, jogo) == PATTERN_FIM) {
+				jogos.add(jogo);
+				jogo = new Jogo();
+			}
 		}
 		
-
+		return jogos;
 	}
 	
-	private Pattern analisarLinha(String linha) {
-		
-
-		
-		return null;
-	}
-
-	private Pattern descobrirPattern(String linha, Jogo jogo) {
+	private Pattern analisarLinha(String linha, Jogo jogo) {
 		Pattern pattern = null;
 		
 		if (linhaFezMatcher(PATTERN_PACMAN.matcher(linha))) {
-			
+			pattern = PATTERN_PACMAN;
+			jogo.setPacman(linha);
 		} else if (linhaFezMatcher(PATTERN_COMIDA.matcher(linha))) {
 			pattern = PATTERN_COMIDA;
+			jogo.setComida(linha);
 		} else if (linhaFezMatcher(PATTERN_GRID.matcher(linha))) {
 			pattern = PATTERN_GRID;
+			jogo.addGrid(linha);
 		} else if (linhaFezMatcher(PATTERN_FIM.matcher(linha))) {
 			pattern = PATTERN_FIM;
 		}
--- src/main/java/br/com/pacman/model/Jogo.java
+++ src/main/java/br/com/pacman/model/Jogo.java
@@ -1,23 +1,66 @@
 package br.com.pacman.model;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
 import lombok.Data;
 
-public class Jogo {
+public class Jogo implements Serializable {
 
+	private static final long serialVersionUID = -2874522428332270252L;
+	
+	private Pacman pacman;
+	private Comida comida;
+	private List<String> grid;
 	
 	
+	public void setPacman(String pacman) {
+		this.pacman = new Pacman(pacman);
+	}
+	
+	public void setComida(String comida) {
+		this.comida = new Comida(comida);
+	}
+	
+	public void addGrid(String grid) {
+		if (this.grid == null) this.grid = new ArrayList<String>();
+		this.grid.add(grid);
+	}
+	
+	
+	
+	public @Data class Pacman implements Serializable {
+		
+		private static final long serialVersionUID = -3652926977790367274L;
+		
 
-	public @Data class Pacman {
+		private String linha;
+		
+		private String coluna;
+		
+		public Pacman(String pacman) {
+			String[] dimensao = pacman.split(",");
+			this.linha = dimensao[0];
+			this.coluna = dimensao[1];
+		}
+		
+	}
+	
+	public @Data class Comida {
 		
 		private String linha;
 		
 		private String coluna;
 		
-		public Pacman(String linha) {
-			
+		public Comida(String comida) {
+			String[] dimensao = comida.split(",");
+			this.linha = dimensao[0];
+			this.coluna = dimensao[1];
 		}
 		
 	}
+
 }
 
 
