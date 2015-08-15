8e7663004db89f6c083bfa9d45468863625b878a
Julio <jcferreira@gmail.com>
2015-08-14 23:46:11 -0300

    Criei classe para efetuar verificacao do caminho ao pacman ate a comida

---
+++ src/main/java/br/com/pacman/estrategia/PacmanEngine.java
@@ -0,0 +1,11 @@
+package br.com.pacman.estrategia;
+
+import java.io.Serializable;
+
+public class PacmanEngine implements Serializable {
+
+	private static final long serialVersionUID = 8312042664675866410L;
+	
+	
+	
+}
